bdf2e07a938fb6e43cac0d1864ee6026da72cdfb
boooooogey <alitugrulbalci@gmail.com>
2023-05-23 13:20:09 -0400

    new interaction with amplification

--- models/model_pos_attention_calib_sigmoid_interaction.py
+++ models/model_pos_attention_calib_sigmoid_interaction.py
@@ -14,55 +14,53 @@ from fastattention import FastAttention
 from utils import MEME
 from models.template_model import TemplateModel
 
-class InteractionLayer(torch.nn.Module):
-
 class TISFM(TemplateModel):
-  def __init__(self, num_of_response, motif_path, window_size, 
+    def __init__(self, num_of_response, motif_path, window_size, 
                stride = 1, pad_motif=4):
-    super(TISFM, self).__init__(num_of_response, motif_path, window_size, stride, pad_motif)
+        super(TISFM, self).__init__(num_of_response, motif_path, window_size, stride, pad_motif)
 
-    self.sigmoid = Sigmoid()
+        self.sigmoid = Sigmoid()
 
-    self.position_emb = Embedding(int(np.ceil(window_size/2)), 1)
+        self.position_emb = Embedding(int(np.ceil(window_size/2)), 1)
 
-    self.attention_layer = SelfAttentionSparse(4, heads=2)
+        self.attention_layer = SelfAttentionSparse(4, heads=2)
 
-    self.attentionpooling = AttentionPooling1D(self.conv_length * 2, self.out_channels//2, mode = "diagonal")
+        self.attentionpooling = AttentionPooling1D(self.conv_length * 2, self.out_channels//2, mode = "diagonal")
 
-    self.interaction_layer = Linear(in_features=self.out_channels//2, out_features=self.out_channels//2, bias=True)
+        self.interaction_layer = Linear(in_features=self.out_channels//2, out_features=self.out_channels//2, bias=True)
 
-    self.l = -1
+        self.l = -1
 
-  def forward(self, x):
+    def forward(self, x):
 
-    l = x.shape[2]
+        l = x.shape[2]
 
-    if self.l != l:
-        ii_f = torch.arange(l//2, device = x.get_device())
-        ii_r = torch.flip(torch.arange(l - l//2, device = x.get_device()),dims=[0])
+        if self.l != l:
+            ii_f = torch.arange(l//2, device = x.get_device())
+            ii_r = torch.flip(torch.arange(l - l//2, device = x.get_device()),dims=[0])
 
-        self.ii = torch.cat([ii_f, ii_r])
-        self.l = l
+            self.ii = torch.cat([ii_f, ii_r])
+            self.l = l
 
-    pos = self.position_emb(self.ii).view(1,1,-1)
+        pos = self.position_emb(self.ii).view(1,1,-1)
 
-    #positional embedding
-    attention = self.attention_layer(x + pos)
-    x = x + attention
+        #positional embedding
+        attention = self.attention_layer(x + pos)
+        x = x + attention
 
-    x = self.motif_layer(x)
+        x = self.motif_layer(x)
 
-    x = self.sigmoid(x)
-    x = x.view(x.shape[0], x.shape[1]//2, x.shape[2]*2)
+        x = self.sigmoid(x)
+        x = x.view(x.shape[0], x.shape[1]//2, x.shape[2]*2)
 
-    #attention pooling
-    x, _ = self.attentionpooling(x)
-    x = x.view(x.shape[0], x.shape[1])
+        #attention pooling
+        x, _ = self.attentionpooling(x)
+        x = x.view(x.shape[0], x.shape[1])
 
-    interactions = self.sigmoid(self.interaction_layer(x))
-    x = x * interactions
+        interactions = self.sigmoid(self.interaction_layer(x))
+        x = x * interactions
 
-    #regression
-    y = self.linreg(x)
+        #regression
+        y = self.linreg(x)
 
-    return y
+        return y
--- models/model_pos_calib_sigmoid_interaction_w_amplification.py
+++ models/model_pos_calib_sigmoid_interaction_w_amplification.py
@@ -21,30 +21,41 @@ class TISFM(TemplateModel):
 
     self.sigmoid = Sigmoid()
 
-    self.position_emb = Embedding(self.conv_length * 2, 1)
-
-    self.attention_interaction = torch.nn.MultiheadAttention(self.out_channels, 19, kdim = 64, batch_first = True)
+    self.position_emb = Embedding(int(np.ceil(window_size/2)), 1)
 
     self.attentionpooling = AttentionPooling1D(self.conv_length * 2, self.out_channels//2, mode = "diagonal")
+
+    self.interaction_layer = Linear(in_features=self.out_channels//2, out_features=self.out_channels//2, bias=True)
 
     self.l = -1
 
   def forward(self, x):
 
+    l = x.shape[2]
+
+    if self.l != l:
+        ii_f = torch.arange(l//2, device = x.get_device())
+        ii_r = torch.flip(torch.arange(l - l//2, device = x.get_device()),dims=[0])
+
+        self.ii = torch.cat([ii_f, ii_r])
+        self.l = l
+
+    pos = self.position_emb(self.ii).view(1,1,-1)
+
+    #positional embedding
+    x = x + pos
+
     x = self.motif_layer(x)
 
     x = self.sigmoid(x)
-
-    #attention on convolution output
-    pos = self.position_emb(torch.arange(x.shape[2], device = x.get_device())).view(1,1,-1)
-    v = (x + pos).transpose(1,2)
-    x = x + self.attention_interaction(x.transpose(1,2), v, v, need_weights=False).transpose(1,2)
-
     x = x.view(x.shape[0], x.shape[1]//2, x.shape[2]*2)
 
     #attention pooling
     x, _ = self.attentionpooling(x)
     x = x.view(x.shape[0], x.shape[1])
+
+    interactions = self.sigmoid(self.interaction_layer(x))
+    x = x * interactions + (1 - x) * interactions
 
     #regression
     y = self.linreg(x)
